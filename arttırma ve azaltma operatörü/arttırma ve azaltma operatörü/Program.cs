572e7e5485008471a4fd86983876ca38af8c1068
Okan Çelebi <okancelebi06@gmail.com>
2023-02-02 23:00:56 +0300

    Add files via upload

---
+++ arttırma ve azaltma operatörü/arttırma ve azaltma operatörü/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arttırma_ve_azaltma_operatörü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arttırma operatörü : ++
+            //Azaltma operatörü : --
+            int sayi1 = 30;
+            sayi1++;
+            Console.WriteLine(sayi1);
+            Console.ReadLine();
+        }
+    }
+}
